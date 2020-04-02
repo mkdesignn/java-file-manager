d6209de3a8dfd6ea010864bc54135acd47c98409
imany <imany.apk@gmail.com>
2020-04-02 21:12:13 +0430

    Fix environment (temporarily)

--- src/test/groovy/filehandler/project/Unit/MailSpec.groovy
+++ src/test/groovy/filehandler/project/Unit/MailSpec.groovy
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockMultipartFile
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -16,6 +17,7 @@ import spock.lang.Specification
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@ActiveProfiles("dev")
 @AutoConfigureMockMvc
 class MailSpec extends Specification {
 
