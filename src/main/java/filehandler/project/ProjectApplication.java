2efbf4a2a5dc9b02915ffc73be6a59b43b5e2895
Mohammad Kaab <mohammad.kaab@gmail.com>
2020-04-11 14:44:10 +0300

    Add procfile

--- src/main/java/filehandler/project/ProjectApplication.java
+++ src/main/java/filehandler/project/ProjectApplication.java
@@ -3,24 +3,31 @@ package filehandler.project;
 import filehandler.project.configuration.AWSProperties;
 import filehandler.project.configuration.File;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableConfigurationProperties({File.class, AWSProperties.class})
-public class ProjectApplication {
+public class ProjectApplication implements CommandLineRunner{
+
 
     @Autowired
     private DataSource dataSource;
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProjectApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(System.getenv("AWS_KEY"));
     }
 }
